9e7af5b67c8ff15d7756191a3f077cf4ed713011
Leela Venkaiah G <leelavg@thoughtexpo.com>
2023-01-09 09:51:15 +0530

    a simple red sphere

--- main.zig
+++ main.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
 const math = std.math;
 const stdout = std.io.getStdOut().writer();
+const print = std.debug.print;
 
 // base type for all vector operations and identifiers
-// return type for all functionsl operating on vector
+// return type for all functions operating on vector
 const vec = @Vector(3, f32);
 
 // any adhoc vector type
@@ -35,13 +36,33 @@ fn expand(val: f32) vec {
 
 // return unit vector along a direction
 fn getUnitVec(dir: vec) vec {
-    return dir / expand(math.sqrt(@reduce(.Add, dir * dir)));
+    return dir / expand(math.sqrt(getDotPro(dir, dir)));
+}
+
+// return dot product of two vectors
+fn getDotPro(v1: vec, v2: vec) f32 {
+    return @reduce(.Add, v1 * v2);
+}
+
+fn hitSphere(center: point3, radius: f32, r: ray) bool {
+    // t^2b.b + 2tb.(A-C) + (A-C).(A-C) - r^2 = 0
+    // solve for t
+    const oc = r.orig - center;
+    const a = getDotPro(r.dir, r.dir);
+    const b = 2.0 * getDotPro(oc, r.dir);
+    const c = getDotPro(oc, oc) - radius * radius;
+    const discriminant = b * b - 4 * a * c;
+    // positive = 2 roots, exact zero = 1 root, negative = 0 roots
+    // roots == number of places that ray hits the sphere
+    return (discriminant > 0);
 }
 
 // returns background color, a simple gradient
 fn rayColor(r: ray) color {
+    if (hitSphere(point3{ 0, 0, -1 }, 0.5, r)) return color{ 1, 0, 0 };
     const unitDir = getUnitVec(r.dir);
     const t: f32 = 0.5 * (unitDir[1] + 1.0);
+    // blendedvalue = (1-t)*startVal + t*endVal
     return expand(1 - t) * color{ 1.0, 1.0, 1.0 } + expand(t) * color{ 0.5, 0.7, 1.0 };
 }
 
